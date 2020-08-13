6087bda7ad5b386aebe72f584882fad293924f21
AdamOlsson <Olsson.95_@hotmail.com>
2020-08-13 16:23:09 +0200

    Project structure

--- Datasets/GraphDataset.py
+++ Datasets/GraphDataset.py
@@ -1,5 +1,13 @@
 from torch.utils.data import Dataset
 
+# Create graph-dataset
+# https://pytorch-geometric.readthedocs.io/en/latest/notes/create_dataset.html
+
+# create_graph as an example
+# https://github.com/dawidejdeholm/dj_graph/blob/master/Utils/SECParser.py
+
+# install
+# https://networkx.github.io/
 
 class GraphDataset(Dataset):
     def __init__(self):
@@ -9,5 +17,7 @@ class GraphDataset(Dataset):
         return len(self.annotations)
 
     def __getitem__(self, idx):
+        def construct_graph():
+            pass
         pass
 
