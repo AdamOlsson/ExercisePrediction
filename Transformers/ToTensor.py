cdcd5a829586c7a3067dd74346ef954749ff083a
AdamOlsson <olsson.95_@hotmail.com>
2020-09-07 10:41:08 +0200

    Casts model and loaded data to device

--- Transformers/ToTensor.py
+++ Transformers/ToTensor.py
@@ -3,13 +3,14 @@ import torch
 class ToTensor(object):
     """Convert ndarrays to Tensors."""
 
-    def __init__(self, dtype=torch.float32, requires_grad=False):
+    def __init__(self, dtype=torch.float32, requires_grad=False, device="cpu"):
         self.dtype = dtype
         self.requires_grad = requires_grad
+        self.device = device
 
     def __call__(self, sample):
         
         data = sample['data']
 
-        sample['data'] = torch.tensor(data, dtype=self.dtype, requires_grad=self.requires_grad)
+        sample['data'] = torch.tensor(data, dtype=self.dtype, requires_grad=self.requires_grad).to(self.device)
         return sample
--- train.py
+++ train.py
@@ -13,17 +13,19 @@ from torchvision.transforms import Compose
 
 def main(annotations_path):
 
-    transform = [ToTensor(dtype=torch.float32, requires_grad=False)]
+    device = "cuda"
+
+    transform = [ToTensor(dtype=torch.float32, requires_grad=False, device=device)]
     dataset = GeneralDataset(annotations_path, np.load, transform=Compose(transform))
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=0)
     
     graph_cfg = {"layout":"openpose", "strategy":"spatial"}
-    model = ST_GCN_18(3, len(dataset.labels), graph_cfg, edge_importance_weighting=True, data_bn=True)
+    model = ST_GCN_18(3, len(dataset.labels), graph_cfg, edge_importance_weighting=True, data_bn=True).to(device)
 
     for i_batch, sample_batched in enumerate(dataloader):
         video = sample_batched["data"]
         out = model(video)
-        
+
         print(out)
         exit()
 
